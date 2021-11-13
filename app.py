4d2060dc6aa75268f33baab95b277bb2c8c12373
Mkrouma <mamadoukrouma@outlook.com>
2021-11-13 10:38:15 +0100

    model with sklearn 1.0.1, flask app

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route("/predict", methods=["POST"])
+def index():
+    # Check if request has a JSON content
+    if request.json:
+        # Get the JSON as dictionnary
+        req = request.get_json()
+        print(f"request : {req}")
+        print(req.keys())
+
+        # Check mandatory key
+        if "input" in req.keys():
+            # Load model
+            reg = joblib.load("model.joblib")
+
+            # Predict
+            prediction = reg.predict(req["input"])
+            print(prediction)
+            # Return the result as JSON but first we need to transform the
+            # result so as to be serializable by jsonify()
+            # prediction = prediction
+
+            return jsonify({"predict": prediction}), 200
+    return jsonify({"msg": "Error: not a JSON or no email key in your request"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
