2e31d4c6966bc38b11be1e12008a07a45a5a40ea
Mkrouma <mamadoukrouma@outlook.com>
2021-11-13 12:59:07 +0100

    new app with flask-restx done in local

--- app.py
+++ app.py
@@ -1,41 +1,64 @@
 import os
 import joblib
 import numpy as np
-from flask_restx import Api
-from flask import Flask, request, jsonify, render_template
+from flask_restx import Resource, Api
+from flask import Flask, request, jsonify, make_response
 
 app = Flask(__name__)
+api = Api(app, version='1.0', title='White Winne Prediction',
+    description='Jedha ML production project.',
+)
 
-# Html documentation
-@app.route("/")
-def index():
-    return render_template("index.html")
-    
+@api.route('/predict', methods=["POST"])
+class Predict(Resource):
+    def post(self):
+        # Check if request has a JSON content
+        if request.json:
+            # Get the JSON as dictionnary
+            req = request.get_json()
+            print(f"request : {req}")
+            print(req.keys())
+
+            # Check mandatory key
+            if "input" in req.keys():
+                # Load model
+                model_file = os.path.join("./model/model.joblib")
+                reg = joblib.load(model_file)
+
+                # Predict
+                prediction = reg.predict(req["input"])
+                prediction = prediction.tolist()
+
+                # return prediction
+                # return jsonify({"predict": prediction}), 200
+                data = {"predict": prediction}
+                return make_response(jsonify(data), 200)
+        return jsonify({"msg": "Error: not a JSON or no email key in your request"})
 
 
-# Predict endpoint
-@app.route("/predict", methods=["POST"])
-def endpoint():
-    # Check if request has a JSON content
-    if request.json:
-        # Get the JSON as dictionnary
-        req = request.get_json()
-        print(f"request : {req}")
-        print(req.keys())
+# # Predict endpoint
+# @app.route("/predict", methods=["POST"])
+# def endpoint():
+#     # Check if request has a JSON content
+#     if request.json:
+#         # Get the JSON as dictionnary
+#         req = request.get_json()
+#         print(f"request : {req}")
+#         print(req.keys())
 
-        # Check mandatory key
-        if "input" in req.keys():
-            # Load model
-            model_file = os.path.join("./model/model.joblib")
-            reg = joblib.load(model_file)
+#         # Check mandatory key
+#         if "input" in req.keys():
+#             # Load model
+#             model_file = os.path.join("./model/model.joblib")
+#             reg = joblib.load(model_file)
 
-            # Predict
-            prediction = reg.predict(req["input"])
-            prediction = prediction.tolist()
+#             # Predict
+#             prediction = reg.predict(req["input"])
+#             prediction = prediction.tolist()
 
-            # return prediction
-            return jsonify({"predict": prediction}), 200
-    return jsonify({"msg": "Error: not a JSON or no email key in your request"})
+#             # return prediction
+#             return jsonify({"predict": prediction}), 200
+#     return jsonify({"msg": "Error: not a JSON or no email key in your request"})
 
 
 if __name__ == "__main__":
