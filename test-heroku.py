bac82bd4b306cdd8025fb6f3c81421b35f3436ef
Mkrouma <mamadoukrouma@outlook.com>
2021-11-13 12:14:21 +0100

    test req

--- test-heroku.py
+++ test-heroku.py
@@ -5,4 +5,4 @@ app_url = "https://jedhamlproduction.herokuapp.com/predict"
 response = requests.post(app_url, json={
     "input": [[7.0, 0.27, 0.36, 20.7, 0.045, 45.0, 170.0, 1.001, 3.0, 0.45, 8.8]]
 })
-print(response.json())
+print(response.headers)
